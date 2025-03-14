7125b52321ba403252d2e5da304c3b815adcb8c7
am1444 <amdoo10@icloud.com>
2025-03-14 13:31:01 -0400

    Update fakesh.py
    Made program clear terminal before running to enhance immersion

--- fakesh.py
+++ fakesh.py
@@ -1,8 +1,12 @@
-from os import system as execmd
-hn = input("cfg:") #.rstrip()
-crSh = input("sh_choice:") #.rstrip()
+hn = input("cfg:") # prompt for the... prompt??? help my comments pls
+crSh = input("sh_choice:") # prompt for shell to mimic
 
-#zsh: command not found: ajsdf
+from os import name,system
+if name=='posix': # clear screen
+    system('clear')
+else:
+    system('cls')
+
 while True:
-    cin = input(hn).split(' ')[0]
-    print(crSh + ": command not found: " + cin)
+    cin = input(hn).split(' ')[0] # user input and get command requested
+    print(crSh + ": command not found: " + cin) # print fake error message lol
